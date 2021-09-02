803b8f16ad3082b3c472570d468b2ce1f2645652
fawazorg <foazorg@gmail.com>
2021-09-02 16:15:41 +0300

    :v: add Block feature

--- Managers/SubscriberManager.js
+++ Managers/SubscriberManager.js
@@ -1,72 +1,119 @@
-const { assign } = require('./util');
-const Client = require('../Client');
-const Requests = require('../Network/IO/Requests');
-const Subscriber = require('../Models/Subscriber/Subscriber');
+const { assign } = require("./util");
+const Client = require("../Client");
+const Requests = require("../Network/IO/Requests");
+const Subscriber = require("../Models/Subscriber/Subscriber");
 
 module.exports = class SubscriberManager {
-    /**
-     * @type {Client}
-     */
-    #Client;
+  /**
+   * @type {Client}
+   */
+  #Client;
 
-    /**
-     * Create a new Subscriber Manager
-     * @param {Client} client 
-     */
-    constructor(client) {
-        this.#Client = client;
-    }
-    
+  /**
+   * Create a new Subscriber Manager
+   * @param {Client} client
+   */
+  constructor(client) {
+    this.#Client = client;
+  }
 
-    /**
-     * Get a Subscriber by their ID
-     * @param {number} id the id of the subscriber
-     * @param {boolean} extended fetch the extended portion of the subscriber profile
-     * @param {boolean} subscribe subscribe to updates to this subscriber's profile
-     * @returns {Subscriber}
-     */
-    GetSubscriber = async (id, extended = false, subscribe = false) => {
-        try {
-            let response = await Requests.SubscriberProfile(this.#Client.V3, id, extended, subscribe);
-            let sub = assign(new Subscriber, response.body);
-            this.#Client.On.Subscriber.Fetched(sub);
-            return sub;
-        } catch { return null }
+  /**
+   * Get a Subscriber by their ID
+   * @param {number} id the id of the subscriber
+   * @param {boolean} extended fetch the extended portion of the subscriber profile
+   * @param {boolean} subscribe subscribe to updates to this subscriber's profile
+   * @returns {Subscriber}
+   */
+  GetSubscriber = async (id, extended = false, subscribe = false) => {
+    try {
+      let response = await Requests.SubscriberProfile(this.#Client.V3, id, extended, subscribe);
+      let sub = assign(new Subscriber(), response.body);
+      this.#Client.On.Subscriber.Fetched(sub);
+      return sub;
+    } catch {
+      return null;
     }
+  };
 
-    /**
-    * Get Subscribers by their IDs
-    * @param {number[]} idList the id of the subscriber
-    * @param {boolean} extended fetch the extended portion of the subscriber profile
-    * @param {boolean} subscribe subscribe to updates to this subscriber's profile
-    * @returns {Subscriber[]}
-    */
-    GetSubscribers = async (idList, extended = false, subscribe = false) => {
-        try {
-            let response = await Requests.SubscriberProfiles(this.#Client.V3, idList, extended, subscribe);
-            let subs = response.map(t => assign(new Subscriber, t));
-            subs.forEach(sub => this.#Client.On.Subscriber.Fetched(sub));
-            return response.map(t => assign(new Subscriber, t));
-        } catch (e) { console.log(e); return []; }
+  /**
+   * Get Subscribers by their IDs
+   * @param {number[]} idList the id of the subscriber
+   * @param {boolean} extended fetch the extended portion of the subscriber profile
+   * @param {boolean} subscribe subscribe to updates to this subscriber's profile
+   * @returns {Subscriber[]}
+   */
+  GetSubscribers = async (idList, extended = false, subscribe = false) => {
+    try {
+      let response = await Requests.SubscriberProfiles(this.#Client.V3, idList, extended, subscribe);
+      let subs = response.map((t) => assign(new Subscriber(), t));
+      subs.forEach((sub) => this.#Client.On.Subscriber.Fetched(sub));
+      return response.map((t) => assign(new Subscriber(), t));
+    } catch (e) {
+      console.log(e);
+      return [];
     }
+  };
 
-    /**
-     * Get Subscriber Settings
-     */
-    GetSettings = async () => {
-        try {
-            return await Requests.SubscriberSettings(this.#Client.V3);
-        } catch { return null }
+  /**
+   * Get Subscriber Settings
+   */
+  GetSettings = async () => {
+    try {
+      return await Requests.SubscriberSettings(this.#Client.V3);
+    } catch {
+      return null;
     }
+  };
 
-    /**
-     * Update the Current Client's Subscriber's Profile
-     * @param {{nickname?: string, status?: string, extended?: { name?: string, about?: string, gender?: number, lookingFor?: number, relationship?: number, langugae?: number, urls?: string[], dateOfBirth?: number}}} data
-     */
-    UpdateSubscriber = async (data) => {
-        try {
-            await Requests.SubscriberUpdate(this.#Client.V3, data);
-            return true;
-        } catch { return false; }
+  /**
+   * Update the Current Client's Subscriber's Profile
+   * @param {{nickname?: string, status?: string, extended?: { name?: string, about?: string, gender?: number, lookingFor?: number, relationship?: number, langugae?: number, urls?: string[], dateOfBirth?: number}}} data
+   */
+  UpdateSubscriber = async (data) => {
+    try {
+      await Requests.SubscriberUpdate(this.#Client.V3, data);
+      return true;
+    } catch {
+      return false;
     }
-}
+  };
+
+  /**
+   * Get block list
+   * @param {boolean} subscribe subscribe to updates to this subscriber's profile
+   */
+  BlockList = async (subscribe = true) => {
+    try {
+      let response = await Requests.SubscriberBlockList(this.#Client.V3, subscribe);
+      return response.body;
+    } catch {
+      return null;
+    }
+  };
+
+  /**
+   * Block subscriber
+   * @param {number} id the id of the subscriber
+   */
+  BlockAdd = async (id) => {
+    try {
+      let response = await Requests.SubscriberBlockAdd(this.#Client.V3, id);
+      return true;
+    } catch {
+      return false;
+    }
+  };
+
+  /**
+   * Delete block from subscriber
+   * @param {number} id the id of the subscriber
+   */
+  BlockDelete = async (id) => {
+    try {
+      let response = await Requests.SubscriberBlockDelete(this.#Client.V3, id);
+      return true;
+    } catch {
+      return false;
+    }
+  };
+};
--- Network/IO/Requests/BlockedRequests.js
+++ Network/IO/Requests/BlockedRequests.js
@@ -1,5 +1,24 @@
-const IO = require('../IO');
+const IO = require("../IO");
 
 module.exports = class BlockedRequests {
+  /**
+   *
+   * @param {IO} io
+   * @param {boolean} subscribe
+   */
+  static SubscriberBlockList = async (io, subscribe = true) => await io.Emit("subscriber block list", { body: { subscribe } });
 
-}
+  /**
+   *
+   * @param {IO} io
+   * @param {Number} id
+   */
+  static SubscriberBlockAdd = async (io, id) => await io.Emit("subscriber block add", { body: { id } });
+
+  /**
+   *
+   * @param {IO} io
+   * @param {Number} id
+   */
+  static SubscriberBlockDelete = async (io, id) => await io.Emit("subscriber block delete", { body: { id } });
+};
